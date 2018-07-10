696f769ab4b37814752c52188eeebb83fa020927
sundy123 <sundy87@126.com>
2018-07-10 16:38:26 +0800

    sundy87
    百度统计API接口demo

---
+++ tongji-api-demo/Config.inc.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Demo of Tongji API
+ * set your information such as USERNAME, PASSWORD ... before use
+ */
+
+//*
+//preLogin,doLogin URL
+define('LOGIN_URL', 'https://api.baidu.com/sem/common/HolmesLoginService');
+
+//Tongji API URL
+define('API_URL', 'https://api.baidu.com/json/tongji/v1/ReportService');
+
+//百度统计账户
+define('USERNAME', 'sundy87');
+
+//百度统计密码
+define('PASSWORD', 'sundy123');
+
+//百度统计网站的TOKEN
+define('TOKEN', 'c55b519bddf7c93cd4dc10f08ff4477f');
+
+//UUID, used to identify your device, for instance: MAC address
+define('UUID', '12231084');
+
+//ACCOUNT_TYPE
+define('ACCOUNT_TYPE', 1); //ZhanZhang:1,FengChao:2,Union:3,Columbus:4
+//*/
